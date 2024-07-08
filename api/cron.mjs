2cfb551fa70743dfc73551777435319d6e7b4e89
horiyuko <0512.hori.yuko@gmail.com>
2024-07-08 19:14:48 +0900

    feat: vercel-setup

---
+++ api/cron.mjs
@@ -0,0 +1,15 @@
+import { checkWeather } from "../src/weather.mjs";
+
+export default async function header(req, res) {
+  if (req.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  try {
+    await checkWeather();
+    res.status(200).json({ message: 'Weather check completed' });
+  } catch (error) {
+    console.error('Error in checkWeather:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
